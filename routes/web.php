e7f0a1d2f9d24b5616a2920f00ee87774a32a754
Marshallramdh <marshall.120140017@student.itera.ac.id>
2024-02-08 15:35:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -40,89 +40,94 @@ Route::get('login/google/callback', [SocialiteController::class, 'callback'])
     ->name('callback');
     Route::get('/home', [HomeController::class, 'index'])->name('admin.index');
 
-    // Route::get('/home', function () {
-    //     return view('home');
-    // })->middleware(['auth'])
-    //   ->name('home');
-
+    
+    //Admin
     Route::middleware(['auth', 'role:1'])->group(function () {
+
         Route::resource('berita', BeritaController::class);
 
+        Route::resource('katalog', KatalogController::class);
+        Route::get('katalog/{katalog}/download', [KatalogController::class, 'download'])->name('katalog.download');
+        
+        Route::resource('tim', TimController::class);
 
+        Route::resource('dokumen', DokumenController::class);
+        Route::get('dokumen/{dokumen}/download', [DokumenController::class, 'download'])->name('dokumen.download');
+
+        Route::resource('pendaftaran', PendaftaranController::class);//create ke user belum
+
+        Route::get('/dashboardadmin', function () {
+            return view('admin.index');
+        });
+
+        Route::get('/adminpembayaran', function () {
+            return view('admin.pembayaran.index');
+        });
+
+        Route::get('/akunadmin', function () {
+            return view('admin.akunadmin.index');
+        });
+        
+        Route::get('/tambahakunadmin', function () {
+            return view('admin.akunadmin.create');
+        });
+        
+        Route::get('/editakunadmin', function () {
+            return view('admin.akunadmin.update');
+        });
     });
+
+    //User
     Route::middleware(['auth', 'role:2'])->group(function () {
         Route::resource('beritas', UserBeritaController::class);
+        Route::resource('isiberita', UserBeritaController::class);
 
+        Route::post('/midtrans/callback', [PaymentController::class, 'handleCallback']);
+        Route::get('/create-payment', [PaymentController::class, 'createPayment']);
 
+        Route::resource('userdokumen', UserDokumenController::class);
+        Route::get('userdokumen/{dokumen}/download', [UserDokumenController::class, 'download'])->name('userdokumen.download');
+
+        Route::resource('userkatalog', UserKatalogController::class);
+        Route::get('userkatalog/{katalog}/download', [UserKatalogController::class, 'download'])->name('userkatalog.download');
+
+        Route::resource('isikatalog', UserKatalogController::class);
+
+        Route::resource('usertim', UserTimController::class);
+
+        Route::get('/kontak', [KontakController::class, 'showForm']);
+        Route::post('/send-email', [KontakController::class, 'sendEmail'])->name('send.email');
+        
+        Route::get('/instagram', [App\Http\Controllers\InstagramController::class, 'index'])->name('kontak');
+
+        Route::get('/sejarah', [App\Http\Controllers\SejarahController::class, 'index'])->name('sejarah');
+
+        Route::get('/layanan', [App\Http\Controllers\LayananController::class, 'index'])->name('layanan');
+
+        Route::get('/pembayaran', [App\Http\Controllers\PembayaranController::class, 'index'])->name('pembayaran');
+
+        Route::get('/statuspendaftaran', [App\Http\Controllers\StatusController::class, 'index'])->name('statuspendaftaran');
+
+        Route::get('/rekap', [App\Http\Controllers\RekapController::class, 'index'])->name('rekap');
+
+        Route::get('/panduan', [App\Http\Controllers\PanduanController::class, 'index'])->name('panduan');
+
+        Route::get('/alur', [App\Http\Controllers\AlurController::class, 'index'])->name('alur');
     });
 
-Route::post('/midtrans/callback', [PaymentController::class, 'handleCallback']);
-Route::get('/create-payment', [PaymentController::class, 'createPayment']);
-
-Route::resource('dokumen', DokumenController::class);
-Route::get('dokumen/{dokumen}/download', [DokumenController::class, 'download'])->name('dokumen.download');
-Route::resource('userdokumen', UserDokumenController::class);
-Route::get('userdokumen/{dokumen}/download', [UserDokumenController::class, 'download'])->name('userdokumen.download');
-
-Route::resource('katalog', KatalogController::class);
-Route::get('katalog/{katalog}/download', [KatalogController::class, 'download'])->name('katalog.download');
-Route::resource('userkatalog', UserKatalogController::class);
-Route::get('userkatalog/{katalog}/download', [UserKatalogController::class, 'download'])->name('userkatalog.download');
 
 
-Route::resource('isikatalog', UserKatalogController::class);
-
-Route::resource('tim', TimController::class);
-Route::resource('usertim', UserTimController::class);
 
 
-Route::resource('beritas', UserBeritaController::class);
-Route::resource('isiberita', UserBeritaController::class);
-
-Route::resource('pendaftaran', PendaftaranController::class);
 
 
-Route::get('/kontak', [KontakController::class, 'showForm']);
-Route::post('/send-email', [KontakController::class, 'sendEmail'])->name('send.email');
+
+
+
+
+
 // User
 Route::get('/', [App\Http\Controllers\DashboardUserController::class, 'index'])->name('home');
-
-
-
-Route::get('/instagram', [App\Http\Controllers\InstagramController::class, 'index'])->name('kontak');
-
-Route::get('/sejarah', [App\Http\Controllers\SejarahController::class, 'index'])->name('sejarah');
-
-Route::get('/layanan', [App\Http\Controllers\LayananController::class, 'index'])->name('layanan');
-
-Route::get('/pembayaran', [App\Http\Controllers\PembayaranController::class, 'index'])->name('pembayaran');
-
-Route::get('/statuspendaftaran', [App\Http\Controllers\StatusController::class, 'index'])->name('statuspendaftaran');
-
-Route::get('/rekap', [App\Http\Controllers\RekapController::class, 'index'])->name('rekap');
-
-Route::get('/panduan', [App\Http\Controllers\PanduanController::class, 'index'])->name('panduan');
-
-
-
-Route::get('/alur', [App\Http\Controllers\AlurController::class, 'index'])->name('alur');
-
-// Admin 
-Route::get('/dashboardadmin', function () {
-    return view('admin.index');
-});
-
-Route::get('/akunadmin', function () {
-    return view('admin.akunadmin.index');
-});
-
-Route::get('/tambahakunadmin', function () {
-    return view('admin.akunadmin.create');
-});
-
-Route::get('/editakunadmin', function () {
-    return view('admin.akunadmin.update');
-});
 
 Route::get('/adminpanduan', function () {
     return view('admin.panduan.index');
@@ -136,6 +141,4 @@ Route::get('/editpanduan', function () {
     return view('admin.panduan.update');
 });
 
-Route::get('/adminpembayaran', function () {
-    return view('admin.pembayaran.index');
-});
+
